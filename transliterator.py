78c07d87d4d60781224318350118ff4fc82f23fd
Ashish Devkota <127653803+adkta@users.noreply.github.com>
2025-11-18 12:18:31 +0545

    Update transliterator.py
    Tokenizer pattern addition

--- transliterator.py
+++ transliterator.py
@@ -10,7 +10,7 @@ from transliteration.utils import get_matching_files
 class TranslitDict(dict[str, str]):
 
     _FILE_ENCODING = 'utf-8'
-    PUNCT_SPACE_REGEX = r"[\s\.\?\|!\",।]+"
+    PUNCT_SPACE_REGEX = r"[\s\.\?\|!\",।-]+"
     DEFAULT_HEADERS = ("Word","Transliteration")
     DEFAULT_DELIMITER = ","
 
