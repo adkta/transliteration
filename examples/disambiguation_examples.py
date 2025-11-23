3114abc3f58723ff143387228afc6c0d2b6efde4
Ashish Devkota <new_moonlit@hotmail.com>
2025-11-23 12:33:23 +0000

    Update disambiguation_examples.py: Adding logic for spelling correction

--- examples/disambiguation_examples.py
+++ examples/disambiguation_examples.py
@@ -1,6 +1,10 @@
 from transliteration.disambiguation import get_native_candidates, native_scoring, scoring, language_model_score, combined_scoring
+from symspellpy import Verbosity
+from symspellpy.suggest_item import SuggestItem
 
-def correct_spelling(word:str)-> set[str]:
+def correct_spelling(word:str, sym_spell)-> set[str]:
+    # sugg_item_list: list[SuggestItem] = sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=2, include_unknown=True)
+    # return {sugg_item.term for sugg_item in sugg_item_list}
     return [word]
 
 def get_words_from_sentence(sentence: str)-> list:
@@ -12,16 +16,20 @@ def get_native_sentence(score_map: dict[str, dict[str, float]]) -> str:
         native_sentence.append(max(score_map[red_word], key=score_map[red_word].get))
     return " ".join(native_sentence)
 
-def disambiguate(sentence: str, model, reverse_dict) -> str:
+def disambiguate(sentence: str, model, reverse_dict, sym_spell, lang_scoring:bool = True) -> str:
     words = get_words_from_sentence(sentence)
     nativ_scored_map: dict[str, dict[str, float]] = dict() #values will also have native scoring. Therefore we opt for dict data type for values
     for word in words:
-        spell_corrs = correct_spelling(word)
+        spell_corrs = correct_spelling(word, sym_spell)
         candidates = get_native_candidates(spell_corrs, reverse_dict)
         scored_candidates = native_scoring(candidates)
         nativ_scored_map[word] = scored_candidates
 
-    norm_lang_scored_map = scoring(nativ_scored_map, window_size = 5)
+    norm_lang_scored_map = dict()
+    if lang_scoring:
+        norm_lang_scored_map = scoring(nativ_scored_map, window_size = 5)
+    else:
+        norm_lang_scored_map = nativ_scored_map
     lang_model_score_map = language_model_score(nativ_scored_map, window_size = 5, model=model)
     combined_score_map = combined_scoring(norm_lang_scored_map, lang_model_score_map)
     return get_native_sentence(combined_score_map)
